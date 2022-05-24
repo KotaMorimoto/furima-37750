cee76a354273cfbea4a017d75423ad9b290e478b
mkota <kota.morimoto.9nov@gmail.com>
2022-05-24 22:57:51 +0900

    rubocopによるコードの整形

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.initials(number: 2)}
-    email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
-    first_name            {'名前なまえナマエ'}
-    last_name             {'名字みょうじミョウジ'}
-    first_name_kana       {'ナマエ'}
-    last_name_kana        {'ミョウジ'}
-    birthday              {'1989-11-09'}
+    nickname              { Faker::Name.initials(number: 2) }
+    email                 { Faker::Internet.free_email }
+    password              { Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+    first_name            { '名前なまえナマエ' }
+    last_name             { '名字みょうじミョウジ' }
+    first_name_kana       { 'ナマエ' }
+    last_name_kana        { 'ミョウジ' }
+    birthday              { '1989-11-09' }
   end
 end
