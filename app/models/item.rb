d9bc7af2e745b1322e97ad9628e8df433f1b8c6b
mkota <kota.morimoto.9nov@gmail.com>
2022-05-26 22:27:20 +0900

    ShipmentFeeモデルの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,8 +4,10 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
+  belongs_to :shipment_fee
 
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipment_fee_id, numericality: { other_than: 1 , message: "can't be blank"}
   
 end
