7e3a2ed2f5c09912609d23afce69825c8c673142
mkota <kota.morimoto.9nov@gmail.com>
2022-05-26 22:46:07 +0900

    LeadTimeクラスの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,10 +6,12 @@ class Item < ApplicationRecord
   belongs_to :condition
   belongs_to :shipment_fee
   belongs_to :prefecture
+  belongs_to :lead_time
 
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :shipment_fee_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :prefecture_id, numericality: { other_than: 0 , message: "can't be blank"}
+  validates :lead_time_id, numericality: { other_than: 1 , message: "can't be blank"}
   
 end
