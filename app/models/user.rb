cef65d07abf6a244ba7cf13eed307098f9695096
mkota <kota.morimoto.9nov@gmail.com>
2022-05-26 21:45:38 +0900

    route設定とモデル追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,6 @@ class User < ApplicationRecord
   validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :birthday, presence: true
+
+  has_many :items
 end
