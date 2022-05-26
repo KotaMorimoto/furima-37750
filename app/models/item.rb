c7d482701aaa78287636d3187ae48e97d8829266
mkota <kota.morimoto.9nov@gmail.com>
2022-05-26 22:42:27 +0900

    Prefectureクラスの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,9 +5,11 @@ class Item < ApplicationRecord
   belongs_to :category
   belongs_to :condition
   belongs_to :shipment_fee
+  belongs_to :prefecture
 
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :shipment_fee_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 0 , message: "can't be blank"}
   
 end
