95802db7e0837a4619bddcad6ac966c76962ea55
mkota <kota.morimoto.9nov@gmail.com>
2022-05-28 14:39:20 +0900

    ActiveStorageの導入と保存機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,5 +3,16 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new 
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :condition_id, :shipment_fee_id, :prefecture_id, :lead_time_id, :price, :image).merge(user_id: current_user.id)
   end
 end
