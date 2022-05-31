fb3b52452b5f1e98f192c442563f77f66b7f8ecf
mkota <kota.morimoto.9nov@gmail.com>
2022-05-31 22:37:19 +0900

    指摘事項の修正、テストケースの追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -62,15 +62,20 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include 'Price is not a number'
       end
-      it '価格は300~9,999,999の範囲でしか登録できない(大きい場合)' do
+      it '価格は9,999,999円を超えると出品できない' do
         @item.price = 10_000_000
         @item.valid?
         expect(@item.errors.full_messages).to include 'Price must be less than or equal to 9999999'
       end
-      it '価格は300~9,999,999の範囲でしか登録できない(小さい場合)' do
+      it '価格は300円未満では出品できない' do
         @item.price = 299
         @item.valid?
         expect(@item.errors.full_messages).to include 'Price must be greater than or equal to 300'
+      end
+      it 'userが紐づいていないと出品できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include 'User must exist'
       end
     end
   end
