c237119adf95f2e6098fbd841e30def88702b8f4
mkota <kota.morimoto.9nov@gmail.com>
2022-05-31 22:35:22 +0900

    指摘事項の修正、初回読み込み時にloadが動かないバグ修正

--- app/javascript/tax.js
+++ app/javascript/tax.js
@@ -3,10 +3,10 @@ function tax(){
   addTaxPrice = document.getElementById("add-tax-price");
   profit = document.getElementById("profit")
   itemPrice.addEventListener("keyup", (e) => {
-    let tax = Math.round(itemPrice.value*0.1);
+    let tax = Math.floor(itemPrice.value*0.1);
     addTaxPrice.innerHTML = tax;
     profit.innerHTML = itemPrice.value - tax;
   });
 }
 
-window.addEventListener('load', tax);
+window.addEventListener('turbolinks:load', tax);
