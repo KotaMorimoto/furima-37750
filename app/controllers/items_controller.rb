f06f152c890f62bbc89ab59ab391b58763665919
mkota <kota.morimoto.9nov@gmail.com>
2022-06-04 16:42:30 +0900

    指摘事項の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,7 +35,7 @@ class ItemsController < ApplicationController
 
   def destroy
     item = Item.find(params[:id])
-    item.destroy if current_user.id == item.user_id
+    item.destroy if user_signed_in? && current_user.id == item.user_id
     redirect_to root_path
   end
 
