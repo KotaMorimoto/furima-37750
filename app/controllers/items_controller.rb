77621142d1f44f7275dfa8aecf65410955361655
mkota <kota.morimoto.9nov@gmail.com>
2022-06-04 15:04:04 +0900

    指摘事項の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,16 +19,16 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+    @item = find_record(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
+    @item = find_record(params[:id])
     redirect_to root_path unless current_user.id == @item.user_id
   end
 
   def update
-    @item = Item.find(params[:id])
+    @item = find_record(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -41,5 +41,9 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name, :description, :category_id, :condition_id, :shipment_fee_id, :prefecture_id,
                                  :lead_time_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def find_record(id)
+    return Item.find(id)
   end
 end
