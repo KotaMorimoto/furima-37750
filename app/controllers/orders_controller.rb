ffd855293d66ecd79a5969675b9afa87c2cb4ea3
mkota <kota.morimoto.9nov@gmail.com>
2022-06-11 11:48:27 +0900

    商品購入機能の実装（クレカ情報除く）

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,26 @@
 class OrdersController < ApplicationController
+  before_action :set_item, only: [:index, :create]
   def index
+    @order_shipment = OrderShipment.new
+  end
+
+  def create
+    @order_shipment = OrderShipment.new(shipment_params)
+    if @order_shipment.valid?
+      @order_shipment.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def shipment_params
+    params.require(:order_shipment).permit(:zip_code, :prefecture_id, :city, :branch, :building, :phone).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 end
